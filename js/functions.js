060a6fe1eb5ed36dfff3bae019ec3ce152237e1c
Lucas Macedo <lucasmacedo123@gmail.com>
2021-03-26 08:31:52 -0300

    first commit

---
+++ js/functions.js
@@ -0,0 +1,48 @@
+// DOM Element's
+const counters = document.querySelectorAll('.counter');
+
+/*** Using forEach() ***/
+
+/*
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const speed = 5000;
+
+        const inc = target / speed;
+
+        if(count < target) {
+            counter.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, 1);
+        } else {
+            counter.innerText = target;
+        }
+    }
+
+    updateCount();
+})
+
+*/
+
+/*** Same functionality, now using for...of ***/
+
+for(let n of counters) {
+    const updateCount = () => {
+        const target = + n.getAttribute('data-target');
+        const count = + n.innerText;
+        const speed = 5000;
+        
+        const inc = target / speed; 
+
+        if(count < target) {
+            n.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, 1);
+        } else {
+            n.innerText = target;
+        }
+    }
+
+    updateCount();
+}
+
