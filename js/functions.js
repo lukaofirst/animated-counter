5f8450e208ef1c8d98603619ac2ded389286d729
lukaofirst <lucasmacedo123@gmail.com>
2022-04-03 17:32:51 -0300

    fixing bug logic related to speed rate

--- js/functions.js
+++ js/functions.js
@@ -4,45 +4,47 @@ const counters = document.querySelectorAll('.counter');
 /*** Using forEach() ***/
 
 /*
-counters.forEach(counter => {
+
+counters.forEach((counter) => {
     const updateCount = () => {
         const target = +counter.getAttribute('data-target');
         const count = +counter.innerText;
-        const speed = 5000;
+        const divider = 5000;
+        const speed = 1000;
 
-        const inc = target / speed;
+        const inc = target / divider;
 
-        if(count < target) {
+        if (count < target) {
             counter.innerText = Math.ceil(count + inc);
-            setTimeout(updateCount, 1);
+            setTimeout(updateCount, speed);
         } else {
             counter.innerText = target;
         }
-    }
+    };
 
     updateCount();
-})
+});
 
 */
 
 /*** Same functionality, now using for...of ***/
 
-for(let n of counters) {
+for (let n of counters) {
     const updateCount = () => {
-        const target = + n.getAttribute('data-target');
-        const count = + n.innerText;
-        const speed = 5000;
-        
-        const inc = target / speed; 
+        const target = +n.getAttribute('data-target');
+        const count = +n.innerText;
+        const divider = 5000;
+        const speed = 1000; // 1000 millisecond => 1 second;
 
-        if(count < target) {
+        const inc = target / divider;
+
+        if (count < target) {
             n.innerText = Math.ceil(count + inc);
-            setTimeout(updateCount, 1);
+            setTimeout(updateCount, speed);
         } else {
             n.innerText = target;
         }
-    }
+    };
 
     updateCount();
 }
-
